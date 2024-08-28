576a7d4ad0aac22b573edbdcebc926f784e5822b
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-08-28 23:29:32 +0530

    added git ignore file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function query(data) {
         "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
         {
             headers: {
-                Authorization: "Bearer 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",
+                Authorization: process.env.HF_TOKEN,
                 "Content-Type": "application/json",
             },
             method: "POST",
